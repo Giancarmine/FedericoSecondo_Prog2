e8a0a0dd425e4d1191a20fa6fd631d21a54344c8
Giancarmine <giancarmine.cuofano@gmail.com>
2015-04-28 10:12:50 +0200

    Started 3^ Exercise

---
+++ Esercizi11Maggio/3 - Lista Ordinata-C++/Lista.h
@@ -0,0 +1,7 @@
+
+typedef struct nodo{
+    int info;
+    struct nodo *link;
+} Nodo;
+
+void Stampa_Lista(Nodo* Head);
---
+++ Esercizi11Maggio/3 - Lista Ordinata-C++/ListaOrdinata.cpp
@@ -0,0 +1,68 @@
+#include "Console.h"
+#include "Lista.h"
+
+using namespace std;
+
+/*===================================================================================================
+
+Autore  	    : Carmine Cuofano											Matricola: N86001700
+Programma   	: inserisci in Lista Ordinata
+Data    		: 27/04/2015
+
+-----------------------------------------------------------------------------------------------------*/
+
+int main()
+{
+    int Tasto;
+    Nodo *Head;
+
+    Head = new Nodo;
+    Head->link = NULL;
+
+    do
+    {
+        //Pulisco lo schermo -Win/Linux-
+        system(CLEAR);
+
+        //Title
+        cout << "Inserimento Ordinato in lista\n\n";
+
+        //Stampo il menu`
+        cout<<"\t1 - Stampa\n";
+        cout<<"\t2 - Inserisci\n";
+        cout<<"\t3 - Elimina\n";
+        cout<<"\t4 - \n";
+        cout<<"\nESC to exit";
+
+        //Menu`
+        Tasto=_getch();
+        switch(Tasto)
+        {
+            case '1' :
+                system(CLEAR);
+                if (Head != NULL){
+                    Stampa_Lista(Head);
+                }
+                else{
+                    cout<<"La Lista e` vuota";
+                }
+                _getch();
+                break;
+            case '2' :
+                system(CLEAR);
+                _getch();
+                break;
+            case 3 :
+                system(CLEAR);
+                _getch();
+                break;
+            case 4 :
+                system(CLEAR);
+                _getch();
+                break;
+        }
+    }
+    while(Tasto != ESC);
+}
+
+#include "Lista.cpp"
